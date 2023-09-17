802b17f72dfe7cfdd6944a0b0345640072989e9e
beingsie <ada@beingsie.com>
2023-09-17 15:24:18 -0700

    Refactor: Improve handling of YouTube video URLs in the 'feed' view

--- projects/playlist_dashboard/playlist_dashboard/views.py
+++ projects/playlist_dashboard/playlist_dashboard/views.py
@@ -11,7 +11,13 @@ def index():
 def feed():
     video_embeds = []
     for url in video_urls:
-        video_id = url.split("v=")[1]
+        if "youtu.be" in url:
+            # Extract the video_id from the URL
+            video_id = url.split("youtu.be/")[1].split("?")[0]
+        else:
+            # Extract the video_id from the regular YouTube URL
+            video_id = url.split("v=")[1].split("?")[0]
+
         embed_code = f'<iframe width="560" height="315" src="https://www.youtube.com/embed/{video_id}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>'
         video_embeds.append(embed_code)
 
