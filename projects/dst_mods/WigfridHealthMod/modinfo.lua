9e5039063c2759ea7ec3c8eda0114d8ac77e8573
Beingsie <ada@beingsie.com>
2023-12-10 20:41:24 -0800

    feat: add new project dst_mods

---
+++ projects/dst_mods/WigfridHealthMod/modinfo.lua
@@ -0,0 +1,12 @@
+
+name = "Wigfrid Health Boost"
+description = "Increases Wigfrid's health to 500."
+author = "Your Name"
+version = "1.0"
+
+dont_starve_compatible = false
+reign_of_giants_compatible = false
+shipwrecked_compatible = false
+dst_compatible = true
+
+priority = 0
