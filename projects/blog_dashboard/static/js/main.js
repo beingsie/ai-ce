31b3e35d74028349e3a5f3dab61f551a81f3d004
Beingsie <ada@beingsie.com>
2023-12-25 12:57:13 -0800

    feat: add modal logic

--- projects/blog_dashboard/static/js/main.js
+++ projects/blog_dashboard/static/js/main.js
@@ -1,3 +1,38 @@
+// JavaScript for Delete Post Modal
+document.addEventListener('DOMContentLoaded', function() {
+    var deleteLinks = document.querySelectorAll('a.delete-post');
+    var modal = document.getElementById('deleteModal');
+    var closeBtn = document.querySelector('.modal .close');
+    var cancelBtn = document.getElementById('cancelDelete');
+    var confirmDeleteBtn = document.getElementById('confirmDelete');
+
+    deleteLinks.forEach(function(link) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            var postUrl = this.href;
+            modal.style.display = 'block';
+
+            confirmDeleteBtn.onclick = function() {
+                window.location.href = postUrl;
+            };
+        });
+    });
+
+    closeBtn.onclick = function() {
+        modal.style.display = 'none';
+    };
+
+    cancelBtn.onclick = function() {
+        modal.style.display = 'none';
+    };
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    };
+});
+
 // Function to format a date object as "Day Month @ Hour:Minute AM/PM" format
 function formatDateTime(date) {
     const options = { weekday: 'short', month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit', hour12: true };
